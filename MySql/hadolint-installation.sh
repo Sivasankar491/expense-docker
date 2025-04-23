0ae41787f25a456bd603c99bf435ed8925a26042
Sankar491 <sivasankarrcks@gmail.com>
2025-04-23 22:39:48 +0530

    Dockerfile

--- MySql/hadolint-installation.sh
+++ MySql/hadolint-installation.sh
@@ -1,5 +1,5 @@
 cd /opt
-curl -LO https://github.com/hadolint/hadolint/releases/latest/download/hadolint-Linux-x86_64
-chmod +x hadolint-Linux-x86_64
+sudo curl -LO https://github.com/hadolint/hadolint/releases/latest/download/hadolint-Linux-x86_64
+sudo chmod +x hadolint-Linux-x86_64
 sudo mv hadolint-Linux-x86_64 /usr/local/bin/hadolint
 hadolint --version
